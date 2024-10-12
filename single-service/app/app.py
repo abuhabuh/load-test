9c6fd645f569dc206b13a4f941b1c73855ed40e3
John Wang <john.wang@petalcard.com>
2024-10-12 14:55:39 -0400

    updates

--- single-service/app/app.py
+++ single-service/app/app.py
@@ -1,8 +1,19 @@
+import logging
+import time
+import sys
+
 from flask import Flask, request, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)  # Add this line
+handler.setLevel(logging.INFO)  # Set the level for the handler
+logger.addHandler(handler)  # Add the handler to the logger
+
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+
 
 def fibonacci(n):
     if n <= 0:
@@ -14,6 +25,19 @@ def fibonacci(n):
         for _ in range(2, n + 1):
             a, b = b, a + b
         return b
+
+
+@app.route('/wait-n', methods=['GET'])
+def wait_n():
+    n = request.args.get('n', type=int)
+    logger.info(f'Received request for /wait-n: {n}')
+    if n is None:
+        logger.error("Request for /wait-n failed: n is not an integer.")
+        return jsonify({"error": "Please provide an integer as n."}), 400
+    time.sleep(n)
+    logger.info(f"Waited for {n} seconds as requested.")
+    return jsonify({"message": "Waited for {} seconds".format(n)})
+
 
 @app.route('/fibonacci', methods=['GET'])
 def get_fibonacci():
@@ -29,4 +53,4 @@ def get_fibonacci():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', use_reloader=True, debug=True, threaded=False)
--- single-service/load_script.py
+++ single-service/load_script.py
@@ -1,30 +1,34 @@
+from collections import Counter
+
 import asyncio
 import aiohttp
 
+
 # Define the number of requests to send
-NUM_REQUESTS = 50000  # You can change this to any number
+NUM_REQUESTS = 10 # You can change this to any number
 
 async def fetch_fibonacci(session, url):
     try:
         async with session.get(url) as response:
-            if response.status == 200:
-                return True
-            else:
-                return False
+            return response.status
     except Exception as e:
         print(f"Request failed: {e}")
-        return False
+        return None
 
 async def main():
-    url = 'http://localhost:5881/fibonacci?n=30'  # Adjust the URL as needed
+    num_seconds = 1
+    url = f'http://localhost:5881/wait-n?n={num_seconds}'  # Adjust the URL as needed
+    start_time = asyncio.get_event_loop().time()
     async with aiohttp.ClientSession() as session:
         tasks = [fetch_fibonacci(session, url) for _ in range(NUM_REQUESTS)]
         results = await asyncio.gather(*tasks)
 
-        success_count = sum(results)
-        failure_count = NUM_REQUESTS - success_count
+        response_counts = Counter(results)
+        for response, count in response_counts.items():
+            print(f"Response {response} occurred {count} times")
 
-        print(f"Successes: {success_count}, Failures: {failure_count}")
+    end_time = asyncio.get_event_loop().time()
+    print(f"Time taken: {end_time - start_time} seconds")
 
 if __name__ == '__main__':
     asyncio.run(main())
